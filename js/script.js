bf6222ebc07ca8366e8b42f0f6313479af1e4d29
Vinicius Boschi <vinicius.yg@gmail.com>
2022-04-22 14:56:34 -0300

    Ajustes finais responsividade no celular

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 $(".navbar__button").click(function() {
     $(this).toggleClass("active") 
+    $(".body").toggleClass("active")
     $(".navbar__list").toggleClass("active")
     $(".navbar__buttons").toggleClass("active")
 })
